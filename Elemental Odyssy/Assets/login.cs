99aa4b3ffaf603fcc0f02091f2181d60fb1758c1
Khitem Mathlouthi <khitemmathlouthi@Khitems-MacBook-Pro.local>
2023-02-19 18:14:17 +0100

    firstscreen with static login google login fb login

---
+++ Elemental Odyssy/Assets/login.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Networking;
+
+
+public class login : MonoBehaviour
+{  
+    [SerializeField] private string authURL = "http://127.0.0.1:9090/user/signin";
+    [SerializeField] private TMPro.TMP_InputField UsernameInputField;
+    [SerializeField] private TMPro.TMP_InputField PasswordInputField;
+    
+    public void OnLoginClick()
+    {
+
+        StartCoroutine(TryLogin());
+        
+
+    }
+    private IEnumerator TryLogin()
+    {
+        string FirstName = UsernameInputField.text;
+        string Password = PasswordInputField.text;
+        Debug.Log(FirstName + " " + Password);
+        WWWForm LoginForm = new WWWForm();
+        LoginForm.AddField("FirstName",FirstName);
+        LoginForm.AddField("Password",Password);
+       
+        
+        UnityWebRequest request = UnityWebRequest.Post(authURL,LoginForm);
+        var handler = request.SendWebRequest();
+        float startTime = 0.0f;
+        while (!handler.isDone)
+        {
+           startTime += Time.deltaTime;
+            if (startTime > 10.0f) 
+            {
+                break;  
+            }
+            yield return null;
+        }
+        if (request.result == UnityWebRequest.Result.Success) 
+        { 
+            Debug.Log(request.downloadHandler.text);
+        }
+        else
+         {
+            Debug.Log("unable to connect");
+        }
+        yield return null;  
+    }
+}
